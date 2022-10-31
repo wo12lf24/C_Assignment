1517314566b33a6139eee6112ae6625eb86533df
wo12lf24 <wo12lf24@daum.net>
2022-10-31 11:24:27 +0900

    Assignment

--- C/Assignment6/Assignment6-2/main.c
+++ C/Assignment6/Assignment6-2/main.c
@@ -4,7 +4,7 @@ Assignment6-2
 이름:김주형
 */
 #include <stdio.h>
-enum // SUN~SAT을 1~7까지 지정
+typedef enum week // SUN~SAT을 1~7까지 지정
 {
     SUN = 1,
     MON,
@@ -13,16 +13,41 @@ enum // SUN~SAT을 1~7까지 지정
     THU,
     FRI,
     SAT
-};
+} week;
+void Week(int num)
+{
+    switch (num)
+    {
+    case SUN:
+        printf("SUN : %d\n", SUN);
+        return;
+    case MON:
+        printf("MON : %d\n", MON);
+        return;
+    case TUE:
+        printf("TUE : %d\n", TUE);
+        return;
+    case WED:
+        printf("WED : %d\n", WED);
+        return;
+    case THU:
+        printf("THU : %d\n", THU);
+        return;
+    case FRI:
+        printf("SUN : %d\n", FRI);
+        return;
+    case SAT:
+        printf("SAT : %d\n", SAT);
+        return;
+    }
+}
 
 int main(void) // enum을 활용해 출력
 {
-    printf("SUN : %d\n", SUN);
-    printf("MON : %d\n", MON);
-    printf("TUE : %d\n", TUE);
-    printf("WED : %d\n", WED);
-    printf("THU : %d\n", THU);
-    printf("FRI : %d\n", FRI);
-    printf("SAT : %d\n", SAT);
+    week day;
+    for (day = SUN; day <= SAT; day += 1)
+    {
+        Week(day);
+    }
     return 0;
 }
