4a1fa6676525b8bce7e4dbe1049f7a796a0b5719
wo12lf24 <wo12lf24@daum.net>
2022-10-31 11:45:49 +0900

    assignment

---
+++ C/Assignment6/Assignment6-3/main.c
@@ -0,0 +1,26 @@
+/*
+Assignment6-3
+학번:2019202003
+이름:김주형
+*/
+#include <stdio.h>
+typedef enum num
+{
+    ace = 1,
+    jack = 10,
+    queen = 10,
+    king = 10
+} num;
+typedef enum shape
+{
+    spade,
+    club,
+    dia,
+    heart
+} shape;
+
+int main(void)
+{
+
+    return 0;
+}
