d53bbdb3227284d8b873ad0d38f0f0ac69042905
wo12lf24 <wo12lf24@daum.net>
2022-11-17 16:49:44 +0900

    Assignment

--- C/Assignment9/Assignment9-3/main.c
+++ C/Assignment9/Assignment9-3/main.c
@@ -1,45 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+int main(void)
 {
-    FILE *pFile;
-    long lSize;
-    char *buffer;
-    size_t result;
-
-    pFile = fopen("C\\Assignment9\\Assignment9-3\\mystery.txt", "rb");
-    if (pFile == NULL)
+    FILE *fr=fopen("mystery.txt","r");
+    char str[100];
+    for(int i =0;i<5;i++)
     {
-        fputs("File error", stderr);
-        exit(1);
+        fgtes(str, sizeof(str), fr);
+        printf("%s", str);
     }
-
-    // 파일의 크기를 ISize 에 저장한다.
-    fseek(pFile, 0, SEEK_END);
-    lSize = ftell(pFile);
-    rewind(pFile);
-
-    // 전체 파일의 내용을 받을 수 있을 정도의 크기로 메모리를 할당한다.
-    buffer = (char *)malloc(sizeof(char) * lSize);
-    if (buffer == NULL)
-    {
-        fputs("Memory error", stderr);
-        exit(2);
-    }
-
-    // 그 파일의 내용을 버퍼에 저장한다.
-    result = fread(buffer, 1, lSize, pFile);
-    if (result != lSize)
-    {
-        fputs("Reading error", stderr);
-        exit(3);
-    }
-
-    /* 이제 파일의 모든 내용은 버퍼에 들어가게 된다. */
-
-    // 종료
-    fclose(pFile);
-    free(buffer);
+    fclose(fr);
     return 0;
 }
