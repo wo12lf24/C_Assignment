7282e66d052975fd4380b00b1dc4413f78a2e04b
wo12lf24 <wo12lf24@daum.net>
2022-11-22 15:16:29 +0900

    Assignment

--- C/Assignment9/Assignment9-14/main.c
+++ C/Assignment9/Assignment9-14/main.c
@@ -9,7 +9,7 @@ int main()
     char fname2[FILENAME_LEN] = {0};
     char ch, ch1, ch2;
 
-    pritnf("첫번째 파일 이름 : ");
+    printf("첫번째 파일 이름 : ");
     gets(fname1, FILENAME_LEN);
     fopen_s(&fp1, fname1, "w");
     printf("첫번째 파일 내용 입력(종료는 Ctrl + Z) : \n");
